87bc6c049a7eaeaa672d66c2da307f8e7fbdc08c
Dayhana Ramirez <dayhana.ramirez@pragma.com.co>
2023-08-10 10:37:59 -0500

    rollback to info

--- src/main/java/com/pragma/users/api/UsersApiApplication.java
+++ src/main/java/com/pragma/users/api/UsersApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsersApiApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(UsersApiApplication.class, args);
     }
 
